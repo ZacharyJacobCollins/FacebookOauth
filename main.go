11332f0af20c9b51121dbbb305a7009a73b5edf9
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-02-22 10:19:12 -0500

    updates

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func handleFacebook(w http.ResponseWriter, r *http.Request) {
 	//Code grabbed when logging into facebook
 	code := r.URL.Query().Get("code")
 	//Access token using code making this call
+	log.Println(code)
+
 	accessTokReq := "https://graph.facebook.com/v2.3/oauth/access_token?client_id=1698453937058092&redirect_uri=http://zacc.xyz:1337/facebook&client_secret=77d2a0169b92e291bd1a61837953973b&code=" + code
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", accessTokReq, nil)
